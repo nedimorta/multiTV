3375ec4db4600ee1639ec65be0e2c8eb27088088
nedimorta <nedimorta@gmail.com>
2024-09-17 10:20:21 -0700

    reset button is not working in several cases issue is solved

--- static/scripts.js
+++ static/scripts.js
@@ -5,12 +5,12 @@ import { adjustIframeSizes } from './modules/layout.js';
 // Initialize event listeners
 setupEventListeners();
 
-// Make addChannel and resetChannel globally accessible
+// Make addChannel, resetChannel, and adjustIframeSizes globally accessible
 window.addChannel = addChannel;
 window.resetChannel = resetChannel;
-
-// Make adjustIframeSizes globally accessible
 window.adjustIframeSizes = adjustIframeSizes;
 
 // Call adjustIframeSizes on page load
-document.addEventListener('DOMContentLoaded', adjustIframeSizes);
+document.addEventListener('DOMContentLoaded', () => {
+  adjustIframeSizes();
+});
